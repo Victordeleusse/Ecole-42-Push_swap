b5786992d5ec2f90ffbaf406d75aed41cb1667d0
Victor DE LEUSSE <vde-leus@e1r13p9.clusters.42paris.fr>
2022-12-27 19:00:23 +0100

    avancement

--- utils_v5.c
+++ utils_v5.c
@@ -6,40 +6,28 @@
 /*   By: vde-leus <vde-leus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 18:48:16 by vde-leus          #+#    #+#             */
-/*   Updated: 2022/12/27 18:14:50 by vde-leus         ###   ########.fr       */
+/*   Updated: 2022/12/27 18:59:25 by vde-leus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
 
-/* Positionnement de la mediane hors du rang 0 ou 1 de mon stack */
+t_stack_list	*ft_get_last_stack(t_stack_list **stack)
+{
+	t_stack_list	*last_of_stack;
 
-// void	ft_get_stack_ready(t_stack_list **stack_a, t_stack_list **stack_b, \
-// 	int *count)
-// {
-// 	t_stack_list	*begin_a;
-// 	t_stack_list	*last_a;
-// 	int				delta;
+	last_of_stack = *stack;
+	while (last_of_stack->next)
+	{
+		last_of_stack = last_of_stack->next;
+	}
+	return (last_of_stack);
+}
 
-// 	begin_a = *stack_a;
-// 	if (begin_a->index_sorted == 2)
-// 		ft_rotate_a(stack_a, count);
-// 	else if (begin_a->next->index_sorted == 2)
-// 		ft_reverse_rotate_a(stack_a, count);
-// 	begin_a = *stack_a;
-// 	delta = begin_a->index_sorted - begin_a->next->index_sorted;
-// 	if (abs(delta) != 1 || abs(delta) != 4)
-// 	{	
-// 		last_a = *stack_a;
-// 		while (last_a->next)
-// 			last_a = last_a->next;
-// 		ft_swap_a(stack_a, count);
+void	ft_get_ready(t_stack_list **stack_a, t_stack_list **stack_b, int *count)
+{
+	t_stack_list	*begin_a;
+	t_stack_list	*last_a;
 
-// 		ft_rotate_a(stack_a, count);
-// 	}
-// 	else
-// 	{
-// 		ft_push_a_to_b(stack_a, count);
-// 		ft_push_a_to_b(stack_a, count);
-// 	}
-// }
+	
+}
