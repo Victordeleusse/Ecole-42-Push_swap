ec5af5445cf251b8b7ce6230a7592f176310dd2b
Victor DE LEUSSE <vde-leus@e2r11p23.clusters.42paris.fr>
2023-01-17 17:41:40 +0100

    avancement

--- utils_0_3/utils_v3.d
+++
@@ -1,6 +0,0 @@
-utils_0_3/utils_v3.o: utils_0_3/utils_v3.c utils_0_3/../pushswap.h \
-  Libft/libft.h Libft/libft_old.h Libft/get_next_line.h \
-  Libft/ft_printf.h ps_lecture/lecture.h ps_operations/operations.h \
-  algo_10_100/algo_10_100.h algo_100_500/algo_100_500.h \
-  algo_5_10/algo_5_10.h algo_3_5/algo_3_5.h bonus/bonus.h \
-  utils_0_3/utils_0_3.h
